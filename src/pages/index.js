2646106d0b0339c9b1f4ecc1b7874d493d9c8fa3
Abhinav Mehta <61967812+abhinavmehta257@users.noreply.github.com>
2025-01-09 17:13:01 +0000

    trip page and all trip page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,11 @@
 
-import Hero from "./components/hero";
-import Itinerary from "./components/Itinerary";
-
 
 export default function Home() {
 
- const {destination, trip_duration, description, start_date, end_date} = plan.overview;
-  const {itinerary} = plan;
   return (
    <div className="px-[24px] bg-[white] h-full">
-      <div className="py-[16px]">
-        <Hero description={description} destination={destination} trip_duration={trip_duration}/>
-      </div>
-      <Itinerary itinerary={itinerary} start_date={start_date}/>
+      
+      main
    </div>
   );
 }
