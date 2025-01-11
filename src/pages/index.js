1d1a9061fb8d10e6a63ab925dff2057a18eef32b
Abhinav Mehta <61967812+abhinavmehta257@users.noreply.github.com>
2025-01-11 17:01:11 +0000

    ads script added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
+import Head from "next/head";
 
 
 export default function Home() {
 
   return (
+    <>
+    <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9744648621612550"
+     crossorigin="anonymous"></script>
+     <meta name="google-adsense-account" content="ca-pub-9744648621612550"></meta>
+    </Head>
    <div className="px-[24px] bg-[white] h-full">
       
       main
    </div>
+   </>
   );
 }
