50111434f90e63f17daa970a240c0430aa04e538
abhinav <abhinavmehta9958@gmail.com>
2026-06-27 18:54:10 +0530

    changes

--- src/pages/components/ui/Footer.jsx
+++ src/pages/components/ui/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
                     <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                     <ul class="text-gray-500 dark:text-gray-400 font-medium">
                         <li class="mb-4">
-                            <a href="https://www.instagram.com/wanderwise_ai/" class="hover:underline ">Instagram</a>
+                            <a href="https://www.instagram.com/wander_wise_ai/" class="hover:underline ">Instagram</a>
                         </li>
                         
                     </ul>
